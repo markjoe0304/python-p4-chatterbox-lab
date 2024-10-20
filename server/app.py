67869412e32d0dfab74adfd0c0827a10a5db9b66
markjoe0304 <markjoegicheru@gmail.com>
2024-10-20 16:38:33 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -14,13 +14,43 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at).all()
+        messages_dict = [message.to_dict() for message in messages]
 
-@app.route('/messages/<int:id>')
+        return make_response(messages_dict, 200)
+    
+    elif request.method == 'POST':
+        data = request.get_json()
+        message = Message(
+            body=data['body'],
+            username=data['username']
+        )
+
+        db.session.add(message)
+        db.session.commit()
+
+        return make_response(message.to_dict(), 201)
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id==id).first()
+
+    if request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        return make_response( {'deleted': True}, 200 )
+    elif request.method == 'PATCH':
+        data = request.get_json()
+        for attr in data:
+            setattr(message, attr, data[attr])
+
+        db.session.add(message)
+        db.session.commit()
+        return make_response(message.to_dict(), 200)
 
 if __name__ == '__main__':
     app.run(port=5555)
